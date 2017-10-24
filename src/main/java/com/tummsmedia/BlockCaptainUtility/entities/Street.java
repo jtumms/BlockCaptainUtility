fa27eec445950d362320efd366c3a4b8c2814c52
John Tumminelli <john.tumminelli@tummsmedia.com>
2017-10-23 23:16:01 -0400

    added embedded collections for street names and neighborhood subdivisions

---
+++ src/main/java/com/tummsmedia/BlockCaptainUtility/entities/Street.java
@@ -0,0 +1,26 @@
+package com.tummsmedia.BlockCaptainUtility.entities;
+
+import javax.persistence.Embeddable;
+
+/**
+ * Created by john.tumminelli on 10/23/17.
+ */
+@Embeddable
+public class Street {
+    private String streetName;
+
+    public Street() {
+    }
+
+    public Street(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+}
--- src/main/java/com/tummsmedia/BlockCaptainUtility/entities/Streets.java
+++
@@ -1,7 +0,0 @@
-package com.tummsmedia.BlockCaptainUtility.entities;
-
-/**
- * Created by john.tumminelli on 10/23/17.
- */
-public class Streets {
-}
